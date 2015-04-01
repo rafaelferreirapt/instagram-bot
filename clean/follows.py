0fbe01c8e8635bbc24ea500140cc471ae4c6220d
Rafael Ferreira <mail@rafaelferreira.pt>
2015-04-01 23:39:43 +0100

    Script to make unfollow who not follows me

---
+++ clean/follows.py
@@ -0,0 +1,44 @@
+__author__ = 'gipmon'
+import time
+import json
+
+from instagram.client import InstagramAPI
+
+
+class CleanFollows:
+    def __init__(self, config_file):
+        # Loading the configuration file, it has the access_token, user_id and others configs
+        self.config = json.load(config_file)
+
+        # Initializing the Instagram API with our access token
+        self.api = InstagramAPI(access_token=self.config["access_token"])
+
+    def run(self):
+        try:
+            self.follows()
+        except Exception, e:
+            time.sleep(1800)
+            self.follows()
+
+    def follows(self, next_page=None):
+        if next_page is None:
+            follows = self.api.user_follows(user_id=self.config['my_user_id'])
+        else:
+            follows = self.api.user_follows(user_id=self.config['my_user_id'], max_tag_id=next_page)
+
+        for follow in follows[0]:
+            var = self.api.user_relationship(user_id=follow.id)
+            print var.incoming_status
+
+            if var.incoming_status == 'none':
+                self.api.unfollow_user(user_id=follow.id)
+
+            var = self.api.user_relationship(user_id=follow.id)
+            print var.outgoing_status
+
+        return self.follows(follows[1].split("&")[-1:][0].split("=")[1])
+
+
+if __name__ == '__main__':
+    clean = CleanFollows(open("config_clean.json", "r"))
+    clean.run()
--- day_bot/day_bot.py
+++ day_bot/day_bot.py
@@ -7,134 +7,137 @@ from random import randint
 import json
 
 
-#Loading the configuration file, it has the access_token, user_id and others configs
-config_file = open("config_bot.json", "r")
-config = json.load(config_file)
+class DayBot:
+    def __init__(self, config_file, tags_file):
+        # Loading the configuration file, it has the access_token, user_id and others configs
+        self.config = json.load(config_file)
 
-#Loading the tags file, it will be keep up to date while the script is running
-tags_file = open("tags.json", "r")
-tags = json.load(tags_file)
+        # Loading the tags file, it will be keep up to date while the script is running
+        self.tags = json.load(tags_file)
 
-#Log file to output to html the debugging info about the script
-filename = config["path"]+config["prefix_name"]+time.strftime("%d%m%Y")+".html"
-log_file = open(filename, "wb")
+        # file name to save the logs
+        self.filename = self.config["path"] + self.config["prefix_name"] + time.strftime("%d%m%Y") + ".html"
+        # Log file to output to html the debugging info about the script
+        self.log_file = open(self.filename, "wb")
 
-#Initializing the Instagram API with our access token
-api = InstagramAPI(access_token=config["access_token"])
+        # Initializing the Instagram API with our access token
+        self.api = InstagramAPI(access_token=self.config["access_token"])
 
-#Likes per tag rate
-likes_per_tag = math.trunc(min(config["follows_per_hour"], config["likes_per_hour"])/len(tags["tags"]))
+        # Likes per tag rate
+        self.likes_per_tag = math.trunc(min(self.config["follows_per_hour"],
+                                            self.config["likes_per_hour"]) / len(self.tags["tags"]))
 
-#1422403200
+    def log_write(self, to_write):
+        if self.filename != self.config["path"] + self.config["prefix_name"] + time.strftime("%d%m%Y") + ".html":
+            self.log_file.close()
+            self.filename = self.config["path"] + self.config["prefix_name"] + time.strftime("%d%m%Y") + ".html"
+            self.log_file = open(self.filename, "wb")
 
-def insta_write(to_write):
-    global filename, log_file
-    if filename != config["path"]+config["prefix_name"]+time.strftime("%d%m%Y")+".html":
-        log_file.close()
-        filename = config["path"]+config["prefix_name"]+time.strftime("%d%m%Y")+".html"
-        log_file = open(filename, "wb")
+        if isinstance(to_write, list):
+            self.log_file.write(''.join(to_write) + "<br/>")
+        else:
+            self.log_file.write(str(to_write) + "<br/>")
+            self.log_file.flush()
 
-    if isinstance(to_write, list):
-        log_file.write(''.join(to_write)+"<br/>")
-    else:
-        log_file.write(str(to_write)+"<br/>")
-    log_file.flush()
+    def going_sleep(self, timer):
+        sleep = randint(timer, 2 * timer)
+        self.log_write("SLEEP " + str(sleep))
+        time.sleep(sleep)
 
+    def like_and_follow(self, media, likes_for_this_tag):
+        try:
+            var = self.api.user_relationship(user_id=media.user.id)
 
-def going_sleep(timer):
-    #sleep for x seconds
-    sleep = randint(timer, 2*timer)
-    insta_write("SLEEP "+str(sleep))
-    time.sleep(sleep)
+            if self.config["my_user_id"] != media.user.id:
+                self.log_write("--------------")
+                self.log_write(var)
 
+                if var.outgoing_status == 'none':
+                    self.log_write("LIKE RESULT:")
+                    self.log_write(self.api.like_media(media_id=media.id))
 
-def like_and_follow(media):
-    global api, likes_for_this_tag
-    try:
-        var = api.user_relationship(user_id=media.user.id)
+                    self.log_write("FOLLOW RESULT:")
+                    self.log_write(self.api.follow_user(user_id=media.user.id))
 
-        if config["my_user_id"] != media.user.id:
-            insta_write("--------------")
-            insta_write(var)
+                    likes_for_this_tag -= 1
 
-            if var.outgoing_status == 'none':
-                insta_write("LIKE RESULT:")
-                insta_write(api.like_media(media_id=media.id))
-
-                insta_write("FOLLOW RESULT:")
-                insta_write(api.follow_user(user_id=media.user.id))
-
-                likes_for_this_tag -= 1
-
-                going_sleep(config["sleep_timer"])
-            else:
-                going_sleep(config["sleep_timer"]/2)
-
-    except Exception, e:
-        insta_write(str(e))
-        insta_write("GOING SLEEP 30 min")
-        time.sleep(1800)
-        like_and_follow(media)
-
-
-#carregar a ultima data verificada
-f = open('daybot_time', 'r')
-ts = float(f.readline())
-f.close()
-last_date = datetime.datetime.fromtimestamp(ts)
-
-#guardar a data num ficheiro
-f = open('daybot_time', 'w')
-f.write(str(time.time()))
-f.close()
-#1422403200
-
-while True:
-    for tag in tags["tags"].keys():
-        insta_write("--------------------")
-        insta_write("TAG: "+tag)
-        insta_write("--------------------")
-
-        insta_write("--------------------")
-        insta_write("DICTIONARY STATUS:")
-        for keys, values in tags["tags"].items():
-            insta_write(keys)
-            if values is not None:
-                insta_write(values)
-
-        likes_for_this_tag = likes_per_tag
-
-        while likes_for_this_tag > 0 and tags["tags"][tag] != 0:
-                if tags["tags"][tag] is None:
-                    media_tag, tags["tags"][tag] = api.tag_recent_media(tag_name=tag, count=likes_for_this_tag)
+                    self.going_sleep(self.config["sleep_timer"])
                 else:
-                    media_tag, tags["tags"][tag] = api.tag_recent_media(tag_name=tag, count=likes_for_this_tag,
-                                                                max_tag_id=tags["tags"][tag])
+                    self.going_sleep(self.config["sleep_timer"] / 2)
 
-                insta_write("API CALL DONE")
+        except Exception, e:
+            self.log_write(str(e))
+            self.log_write("GOING SLEEP 30 min")
+            time.sleep(1800)
+            return self.like_and_follow(media, likes_for_this_tag)
 
-                #se todos tiverem data superior a carregada morre
-                date_all_higher = False
+        return likes_for_this_tag
 
-                for media in media_tag:
-                    date_all_higher |= (media.created_time > last_date)
-                    #print media.created_time > last_date
+    def run(self):
+        # load the last verified date
+        f = open('daybot_time', 'r')
+        ts = float(f.readline())
+        f.close()
+        last_date = datetime.datetime.fromtimestamp(ts)
 
-                if not date_all_higher:
-                    insta_write("DONE!")
+        # save the date in a file
+        f = open('daybot_time', 'w')
+        f.write(str(time.time()))
+        f.close()
+
+        while True:
+            for tag in self.tags["tags"].keys():
+                self.log_write("--------------------")
+                self.log_write("TAG: " + tag)
+                self.log_write("--------------------")
+
+                self.log_write("--------------------")
+                self.log_write("DICTIONARY STATUS:")
+
+                for keys, values in self.tags["tags"].items():
+                    self.log_write(keys)
+                    if values is not None:
+                        self.log_write(values)
+
+                likes_for_this_tag = self.likes_per_tag
+
+                while likes_for_this_tag > 0 and self.tags["tags"][tag] != 0:
+                    if self.tags["tags"][tag] is None:
+                        media_tag, self.tags["tags"][tag] = self.api.tag_recent_media(tag_name=tag,
+                                                                                      count=likes_for_this_tag)
+                    else:
+                        media_tag, self.tags["tags"][tag] = self.api.tag_recent_media(tag_name=tag,
+                                                                                      count=likes_for_this_tag,
+                                                                                      max_tag_id=self.tags["tags"][tag])
+
+                    self.log_write("API CALL DONE")
+
+                    # if all the tags have date higher than the loaded it dies
+                    date_all_higher = False
+
+                    for media in media_tag:
+                        date_all_higher |= (media.created_time > last_date)
+                        # print media.created_time > last_date
+
+                    if not date_all_higher:
+                        self.log_write("DONE!")
+                        exit(1)
+
+                    if len(media_tag) == 0 or self.tags["tags"][tag] is None:
+                        self.tags["tags"][tag] = 0
+                        likes_for_this_tag = 0
+                    else:
+                        self.log_write(self.tags["tags"][tag])
+                        self.tags["tags"][tag] = self.tags["tags"][tag].split("&")[-1:][0].split("=")[1]
+
+                    for m in media_tag:
+                        # verify if date is higher
+                        likes_for_this_tag = self.like_and_follow(m, likes_for_this_tag)
+
+                if reduce(lambda r, h: r and h[1] == 0, self.tags["tags"].items(), True):
+                    self.log_write("END")
                     exit(1)
 
-                if len(media_tag) == 0 or tags["tags"][tag] is None:
-                    tags["tags"][tag] = 0
-                    likes_for_this_tag = 0
-                else:
-                    insta_write(tags["tags"][tag])
-                    tags["tags"][tag] = tags["tags"][tag].split("&")[-1:][0].split("=")[1]
-
-                for m in media_tag:
-                    #verify if date is higher
-                    like_and_follow(m)
-
-        if reduce(lambda r, h: r and h[1] == 0, tags["tags"].items(), True):
-            insta_write("END")
-            exit(1)
+if __name__ == '__main__':
+    bot = DayBot(open("config_bot.json", "r"), open("tags.json", "r"))
+    bot.run()
